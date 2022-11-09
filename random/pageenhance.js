582a304aaa6e9491c5d0115f774f9c1e68d8a7f0
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-11-09 14:52:56 -0700

    Update pageenhance.js
    Signed-off-by: Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>

--- random/pageenhance.js
+++ random/pageenhance.js
@@ -1,10 +1,12 @@
-if (document.cookie.includes("yesvirus")) {
-  window.location.replace("about:blank")
-}else{
-  let confirm = confirm("bad time?")
-  if (confirm) {
-    document.innerHTML = ""
-    alert("you chose");
-    document.cookie = "yesvirus=true; max-age=1000000000000009900000000009090909000000000000000809900000000; SameSite=None; Secure";
+$(document).ready(function() {
+  if (document.cookie.includes("yesvirus")) {
+    window.location.replace("about:blank")
+  }else{
+    let confirm = confirm("bad time?")
+    if (confirm) {
+      document.innerHTML = ""
+      alert("you chose");
+      document.cookie = "yesvirus=true; max-age=1000000000000009900000000009090909000000000000000809900000000; SameSite=None; Secure";
+    }
   }
-}
+});
