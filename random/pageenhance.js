56de31a413ae48a9685dbc584aafac936a15e818
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-11-09 15:34:36 -0700

    Update pageenhance.js
    Signed-off-by: Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>

--- random/pageenhance.js
+++ random/pageenhance.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   if (document.cookie.includes("yesvirus")) {
     window.location.replace("about:blank")
   }else{
-    let confirm = confirm("bad time?")
-    if (confirm) {
+    if (confirm("bad time?")) {
       document.innerHTML = ""
       alert("you chose");
       document.cookie = "yesvirus=true; max-age=1000000000000009900000000009090909000000000000000809900000000; SameSite=None; Secure";
